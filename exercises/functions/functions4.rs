8c8eeabbdd2a3f9068ef89be8f635787c1a0e1df
Jesse Farmer <jesse@20bits.com>
2022-01-11 16:40:47 -0800

    [DONE] Exercise functions4.rs

--- exercises/functions/functions4.rs
+++ exercises/functions/functions4.rs
@@ -4,14 +4,18 @@
 // This store is having a sale where if the price is an even number, you get
 // 10 Rustbucks off, but if it's an odd number, it's 3 Rustbucks off.
 
-// I AM NOT DONE
+// Hint
+//
+// The error message points to line 14 and says it expects a type after the
+// `->`. This is where the function's return type should be-- take a look at
+// the `is_even` function for an example!
 
 fn main() {
     let original_price = 51;
     println!("Your sale price is {}", sale_price(original_price));
 }
 
-fn sale_price(price: i32) -> {
+fn sale_price(price: i32) -> i32 {
     if is_even(price) {
         price - 10
     } else {
