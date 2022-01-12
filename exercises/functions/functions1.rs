1585556f0bfc362d808a687eb96b57b37639434b
Jesse Farmer <jesse@20bits.com>
2022-01-11 16:37:29 -0800

    [DONE] Exercise functions1.rs

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,8 +1,15 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
+// Hint
+// This main function is calling a function that it expects to exist, but the
+// function doesn't exist. It expects this function to have the name `call_me`.
+// It expects this function to not take any arguments and not return a value.
+// Sounds a lot like `main`, doesn't it?
 
 fn main() {
     call_me();
+}
+
+fn call_me() {
 }
