543d31bef4d61e03d261ea2ec4b233e30b0ad5f0
Jesse Farmer <jesse@20bits.com>
2022-01-12 21:36:33 -0800

    [DONE] Exercise functions5.rs

--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -1,7 +1,16 @@
 // functions5.rs
 // Make me compile! Execute `rustlings hint functions5` for hints :)
 
-// I AM NOT DONE
+// Hint
+//
+// This is a really common error that can be fixed by removing one character.
+// It happens because Rust distinguishes between expressions and statements: expressions return
+// a value based on its operand, and statements simply return a () type which behaves just like `void` in C/C++ language.
+// We want to return a value of `i32` type from the `square` function, but it is returning a `()` type...
+// They are not the same. There are two solutions:
+// 1. Add a `return` ahead of `num * num;`
+// 2. remove `;`, make it to be `num * num`
+
 
 fn main() {
     let answer = square(3);
@@ -9,5 +18,5 @@ fn main() {
 }
 
 fn square(num: i32) -> i32 {
-    num * num;
+    return num * num;
 }
