3d9310c7f1299cf735708059b75671621ebb7e1f
Jesse Farmer <jesse@20bits.com>
2022-01-12 23:27:48 -0800

    [DONE] Exercise modules1.rs

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -1,7 +1,12 @@
 // modules1.rs
 // Make me compile! Execute `rustlings hint modules1` for hints :)
 
-// I AM NOT DONE
+// Hint
+//
+// Everything is private in Rust by default-- but there's a keyword we can use
+// to make something public! The compiler error should point to the thing that
+// needs to be public.
+
 
 mod sausage_factory {
     // Don't let anybody outside of this module see this!
@@ -9,7 +14,7 @@ mod sausage_factory {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
+    pub fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
     }
