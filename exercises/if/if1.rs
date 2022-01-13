e0c14071ead6c6937a2f314e6c9d71b6c47c3061
Jesse Farmer <jesse@20bits.com>
2022-01-12 21:38:01 -0800

    [DONE] Exercise if1.rs

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -1,13 +1,23 @@
 // if1.rs
 
-// I AM NOT DONE
+// Hint
+//
+// It's possible to do this in one line if you would like!
+// Some similar examples from other languages:
+// - In C(++) this would be: `a > b ? a : b`
+// - In Python this would be:  `a if a > b else b`
+// Remember in Rust that:
+// - the `if` condition does not need to be surrounded by parentheses
+// - `if`/`else` conditionals are expressions
+// - Each condition is followed by a `{}` block.
+
 
 pub fn bigger(a: i32, b: i32) -> i32 {
-    // Complete this function to return the bigger number!
-    // Do not use:
-    // - another function call
-    // - additional variables
-    // Execute `rustlings hint if1` for hints
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
 }
 
 // Don't mind this for now :)
