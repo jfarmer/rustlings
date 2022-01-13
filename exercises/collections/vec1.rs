b28c4e5dca48442e4e083002ec38610afb4e7cbf
Jesse Farmer <jesse@20bits.com>
2022-01-12 23:36:25 -0800

    [DONE] Exercise vec1.rs

--- exercises/collections/vec1.rs
+++ exercises/collections/vec1.rs
@@ -4,12 +4,21 @@
 // Make me compile and pass the test!
 // Execute the command `rustlings hint vec1` if you need hints.
 
-// I AM NOT DONE
+// Hint
+//
+// In Rust, there are two ways to define a Vector.
+// 1. One way is to use the `Vec::new()` function to create a new vector
+//   and fill it with the `push()` method.
+// 2. The second way, which is simpler is to use the `vec![]` macro and
+//   define your elements inside the square brackets.
+// Check this chapter: https://doc.rust-lang.org/stable/book/ch08-01-vectors.html
+// of the Rust book to learn more.
+
+
 
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
-    let v = // TODO: declare your vector here with the macro for vectors
-
+    let v = vec![10, 20, 30, 40];
     (a, v)
 }
 
