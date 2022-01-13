dea75815a2aa3d5bee67d025ea8062f01569e68c
Jesse Farmer <jesse@20bits.com>
2022-01-12 22:08:29 -0800

    [DONE] Exercise move_semantics4.rs

--- exercises/move_semantics/move_semantics4.rs
+++ exercises/move_semantics/move_semantics4.rs
@@ -6,10 +6,21 @@
 
 // I AM NOT DONE
 
-fn main() {
-    let vec0 = Vec::new();
+// Hint
+//
+// Stop reading whenever you feel like you have enough direction :) Or try
+// doing one step and then fixing the compiler errors that result!
+// So the end goal is to:
+//    - get rid of the first line in main that creates the new vector
+//    - so then `vec0` doesn't exist, so we can't pass it to `fill_vec`
+//    - we don't want to pass anything to `fill_vec`, so its signature should
+//      reflect that it does not take any arguments
+//    - since we're not creating a new vec in `main` anymore, we need to create
+//      a new vec in `fill_vec`, similarly to the way we did in `main`
 
-    let mut vec1 = fill_vec(vec0);
+
+fn main() {
+    let mut vec1 = fill_vec();
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -20,7 +31,7 @@ fn main() {
 
 // `fill_vec()` no longer takes `vec: Vec<i32>` as argument
 fn fill_vec() -> Vec<i32> {
-    let mut vec = vec;
+    let mut vec = Vec::new();
 
     vec.push(22);
     vec.push(44);
