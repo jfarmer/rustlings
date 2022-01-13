7be3116708c31ff8973f812b4b8da55251bb38df
Jesse Farmer <jesse@20bits.com>
2022-01-12 23:41:18 -0800

    [DONE] Exercise vec2.rs

--- exercises/collections/vec2.rs
+++ exercises/collections/vec2.rs
@@ -7,12 +7,17 @@
 // Execute the command `rustlings hint vec2` if you need
 // hints.
 
-// I AM NOT DONE
+// Hint
+//
+// Hint 1: `i` is each element from the Vec as they are being iterated.
+//   Can you try multiplying this?
+// Hint 2: Check the suggestion from the compiler error ;)
+
+
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
-    for i in v.iter_mut() {
-        // TODO: Fill this up so that each element in the Vec `v` is
-        // multiplied by 2.
+    for el in v.iter_mut() {
+        *el *= 2;
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
