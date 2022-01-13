044acc8fe7348c6a5a222f9909e35f215e2ab3eb
Jesse Farmer <jesse@20bits.com>
2022-01-12 22:51:30 -0800

    [DONE] Exercise primitive_types3.rs

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,19 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
+// Hint
+//
+// There's a shorthand to initialize Arrays with a certain size that does not
+// require you to type in 100 items (but you certainly can if you want!).
+// For example, you can do:
+// let array = ["Are we there yet?"; 10];
+
+// Bonus: what are some other things you could have that would return true
+// for `a.len() >= 100`?
+
 
 fn main() {
-    let a = ???
+    let a = ["Hi"; 200];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
