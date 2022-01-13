a26ab4432e95504ea44821f6a72b45ea30784b28
Jesse Farmer <jesse@20bits.com>
2022-01-12 22:10:46 -0800

    [DONE] Exercise move_semantics5.rs

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -3,13 +3,24 @@
 // adding, changing or removing any of them.
 // Execute `rustlings hint move_semantics5` for hints :)
 
-// I AM NOT DONE
+// Hint
+//
+// Carefully reason about the range in which each mutable reference is in
+// vogue. Does it help to update the value of referent (x) immediately after
+// the mutable reference is taken? Read more about 'Mutable References'
+// in the book's section References and Borrowing':
+// https://doc.rust-lang.org/book/ch04-02-references-and-borrowing.html#mutable-references.
 
 fn main() {
     let mut x = 100;
-    let y = &mut x;
-    let z = &mut x;
-    *y += 100;
-    *z += 1000;
+    {
+        let y = &mut x;
+        *y += 100;
+    }
+    {
+        let z = &mut x;
+        *z += 1000;
+    }
+
     assert_eq!(x, 1200);
 }
