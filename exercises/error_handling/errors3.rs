44b024b161807b27ba873baf84f03d36246100c7
Jesse Farmer <jesse@20bits.com>
2022-01-16 14:43:46 -0800

    [DONE] Exercise errors3.rs

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -4,11 +4,13 @@
 // Why not? What should we do to fix it?
 // Execute `rustlings hint errors3` for hints!
 
-// I AM NOT DONE
+// Hint
+//
+// If other functions can return a `Result`, why shouldn't `main`?
 
 use std::num::ParseIntError;
 
-fn main() {
+fn main() -> Result<(), ParseIntError> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
@@ -20,6 +22,8 @@ fn main() {
         tokens -= cost;
         println!("You now have {} tokens.", tokens);
     }
+
+    Ok(())
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
