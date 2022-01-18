20c869295bce27a9969ba94ff5ffa4752d0eadee
Jesse Farmer <jesse@20bits.com>
2022-01-17 22:20:11 -0800

    [DONE] Exercise errors6.rs

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -8,7 +8,23 @@
 
 // Make these tests pass! Execute `rustlings hint errors6` for hints :)
 
-// I AM NOT DONE
+// Hint
+//
+// This exercise uses a completed version of `PositiveNonzeroInteger` from
+// errors4.
+
+// Below the line that TODO asks you to change, there is an example of using
+// the `map_err()` method on a `Result` to transform one type of error into
+// another. Try using something similar on the `Result` from `parse()`. You
+// might use the `?` operator to return early from the function, or you might
+// use a `match` expression, or maybe there's another way!
+
+// You can create another function inside `impl ParsePosNonzeroError` to use
+// with `map_err()`.
+
+// Read more about `map_err()` in the `std::result` documentation:
+// https://doc.rust-lang.org/std/result/enum.Result.html#method.map_err
+
 
 use std::num::ParseIntError;
 
@@ -23,6 +39,10 @@ impl ParsePosNonzeroError {
     fn from_creation(err: CreationError) -> ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
+
+    fn from_parse_int(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
     // TODO: add another error conversion function here.
 }
 
@@ -31,7 +51,7 @@ fn parse_pos_nonzero(s: &str)
 {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+    let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parse_int)?;
     PositiveNonzeroInteger::new(x)
         .map_err(ParsePosNonzeroError::from_creation)
 }
