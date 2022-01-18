038da252b57a254040aa56b19b3ea48b721f89ce
Jesse Farmer <jesse@20bits.com>
2022-01-17 22:21:33 -0800

    [DONE] Exercise generics1.rs

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -3,9 +3,12 @@
 
 // Execute `rustlings hint generics1` for hints!
 
-// I AM NOT DONE
+// Hint
+//
+// Vectors in rust make use of generics to create dynamically sized arrays of any type.
+// You need to tell the compiler what type we are pushing onto this vector.
 
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
+    let mut shopping_list: Vec<&str> = Vec::new();
     shopping_list.push("milk");
 }
