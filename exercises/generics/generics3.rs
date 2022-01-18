dd4364fbaf7a4e9be1d70e0a0204dc9eba764b51
Jesse Farmer <jesse@20bits.com>
2022-01-17 22:28:42 -0800

    [DONE] Exercise generics3.rs

--- exercises/generics/generics3.rs
+++ exercises/generics/generics3.rs
@@ -10,15 +10,24 @@
 
 // Execute 'rustlings hint generics3' for hints!
 
-// I AM NOT DONE
+// Hint
+//
+// To find the best solution to this challenge you're going to need to think back to your
+// knowledge of traits, specifically Trait Bound Syntax -  you may also need this: "use std::fmt::Display;"
 
-pub struct ReportCard {
-    pub grade: f32,
+// This is definitely harder than the last two exercises! You need to think about not only making the
+// ReportCard struct generic, but also the correct property - you will need to change the implementation
+// of the struct slightly too...you can do it!
+
+use std::fmt::Display;
+
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: std::fmt::Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -46,7 +55,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
